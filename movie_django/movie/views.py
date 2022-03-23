2ba02ce615b070dce7cb39d0d3ee2b54895860c6
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-23 16:40:24 -0700

    scaffold out views.py

--- movie_django/movie/views.py
+++ movie_django/movie/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+# from .models import
 
 # Create your views here.
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
+
+# class Serializer(serializers.HyperlinkedModelSerializer):
+# class Serializer(serializers.HyperlinkedModelSerializer):
