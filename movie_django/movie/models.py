f927a0e7a7e1ffd2571f501551759f55159f7f65
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-24 16:52:14 -0700

    add movie and movie cast model

--- movie_django/movie/models.py
+++ movie_django/movie/models.py
@@ -11,9 +11,26 @@ class Actor(models.Model):
     def __str__(self):
         return self.fname
 
-# movie_cast model
-
 # movie model
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    year = models.IntegerField()
+    time = models.IntegerField()
+    language = models.CharField(max_length=50)
+    release_date = models.DateField(auto_now_add=True)
+    release_country = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.title
+
+# movie_cast model
+class MovieCast(models.model):
+    role = models.CharField(max_length=30)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='movie_cast')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie')
+
+    def __str__(self):
+        return self.role
 
 # movie_genre model
 
