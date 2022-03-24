3ab18472424f8d1277bba008fb18f29199f43398
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-24 16:46:33 -0700

    add actor model

--- movie_django/movie/models.py
+++ movie_django/movie/models.py
@@ -3,6 +3,13 @@ from django.db import models
 # Create your models here.
 
 # actor model
+class Actor(models.Model):
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.fname
 
 # movie_cast model
 
--- movie_django/movie_django/settings.py
+++ movie_django/movie_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
