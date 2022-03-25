091fc572fcc527ffea806e126002d5ed89f6d78b
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-24 17:15:32 -0700

    add to serializers.py

--- movie_django/movie/serializers.py
+++ movie_django/movie/serializers.py
@@ -1,47 +1,62 @@
 from rest_framework import serializers
-# from .models import 
+from .models import Actor, Movie, MovieCast
 
 class ActorSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+    actor = serializers.HyperlinkedRelatedField(view_name='actor_detail', read_only=True)
+    actor_url = serializers.ModelSerializer.serializer_url_field(view_name='actor_detail')
 
-class MovieCastSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        # model = 
-        fields = ()
+        model = Actor
+        fields = ('id', 'fname', 'lname', 'gender', 'actor', 'actor_url')
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    movie = serializers.HyperlinkedRelatedField(view_name='movie_detail', read_only=True)
+    movie_url = serializers.ModelSerializer.serializer_url_field(view_name='movie_detail')
     class Meta:
-        # model = 
+        model = Movie
+        fields = ('id', 'title', 'year', 'time', 'language', 'release_date', 'release_country', 'movie', 'movie_url')
+
+class MovieCastSerializer(serializers.HyperlinkedModelSerializer):
+    movie = serializers.HyperlinkedRelatedField(view_name='movie_detail', read_only=True)
+    movie_id = serializers.PrimaryKeyRelatedField(
+        queryset=Movie.objects.all(),
+        source='movie'
+    )
+    actor = serializers.HyperlinkedRelatedField(view_name='actor_detail', read_only=True)
+    actor_id = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        source='actor'
+    )
+    class Meta:
+        model = MovieCast
         fields = ()
 
-class MovieGenreSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+# class MovieGenreSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = 
+#         fields = ()
 
-class GenreSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+# class GenreSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = 
+#         fields = ()
 
-class RatingSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+# class RatingSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = 
+#         fields = ()
 
-class ReviewerSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+# class ReviewerSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = 
+#         fields = ()
 
-class MovieDirectionSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+# class MovieDirectionSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = 
+#         fields = ()
 
-class DirectorSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        # model = 
-        fields = ()
+# class DirectorSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = 
+#         fields = ()
