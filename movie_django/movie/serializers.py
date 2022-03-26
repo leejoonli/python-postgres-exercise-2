ea0742dc8ec53d9d364f3a25dca87615ea04a43f
jeremiahlee94 <jhwlee94@gmail.com>
2022-03-25 19:40:09 -0700

    make movie genre and genre serializer

--- movie_django/movie/serializers.py
+++ movie_django/movie/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Actor, Movie, MovieCast
+from .models import Actor, Movie, MovieCast, Genre, MovieGenre, Reviewer, Rating, Director, MovieDirection
 
 class ActorSerializer(serializers.HyperlinkedModelSerializer):
     actor = serializers.HyperlinkedRelatedField(view_name='actor_detail', read_only=True)
@@ -31,32 +31,45 @@ class MovieCastSerializer(serializers.HyperlinkedModelSerializer):
         model = MovieCast
         fields = ('id', 'role', 'movie', 'movie_id', 'actor', 'actor_id')
 
-# class MovieGenreSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = 
-#         fields = ()
+class GenreSerializer(serializers.HyperlinkedModelSerializer):
+    genre = serializers.HyperlinkedRelatedField(view_name='genre_detail', read_only=True)
+    genre_url = serializers.ModelSerializer.serializer_url_field(view_name='genre_detail')
 
-# class GenreSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = 
-#         fields = ()
+    class Meta:
+        model = Genre
+        fields = ('id', 'genre', 'genre_url', 'title')
 
-# class RatingSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = 
-#         fields = ()
+class MovieGenreSerializer(serializers.HyperlinkedModelSerializer):
+    movie = serializers.HyperlinkedRelatedField(view_name='movie_detail', read_only=True)
+    movie_id = serializers.PrimaryKeyRelatedField(
+        queryset=Movie.objects.all(),
+        source='movie'
+    )
+    genre = serializers.HyperlinkedRelatedField(view_name='genre_detail', read_only=True)
+    genre_id = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(),
+        source='genre'
+    )
+    class Meta:
+        model = MovieGenre
+        fields = ('id', 'movie', 'movie_id', 'genre', 'genre_id')
 
-# class ReviewerSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = 
-#         fields = ()
+class ReviewerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Reviewer
+        fields = ()
 
-# class MovieDirectionSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = 
-#         fields = ()
+class RatingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ()
 
-# class DirectorSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = 
-#         fields = ()
+class DirectorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Director
+        fields = ()
+
+class MovieDirectionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MovieDirection
+        fields = ()
